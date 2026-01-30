8d7105ed3b7e9576aaf33565019b4f195eac6323
Jordan Matelsky <j6k4m8@gmail.com>
2026-01-29 22:47:07 -0500

    ci: add placehllder test for ci

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,16 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:tidings/app/tidings_app.dart';
-
-// void main() {
-//   testWidgets('Tidings shows onboarding for new users',
-//       (WidgetTester tester) async {
-//     await tester.pumpWidget(const TidingsApp());
-
-//     expect(find.text('Welcome to Tidings'), findsOneWidget);
-//     expect(find.text('Connect IMAP'), findsOneWidget);
-//   });
-// }
+void main() {
+  test('widget_test placeholder', () {
+    expect(true, isTrue);
+  });
+}
